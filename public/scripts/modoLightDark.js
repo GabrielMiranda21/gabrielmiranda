996341f9689b39997367bb969412ccd9886a7450
GabrielMiranda21 <gabrielmirand204@gmail.com>
2025-03-01 15:27:13 -0300

    Eliminando temporariamente o modo dark e light, correção de textos e adaptação para div status em dispositivos móveis, ajuste no menu vertical

--- public/scripts/modoLightDark.js
+++ public/scripts/modoLightDark.js
@@ -1,3 +1,4 @@
+/*
 let desktopContainer = document.querySelector(".desktop-container");
 let modo = desktopContainer.querySelector(".modo");
 let claroEscuro = modo.querySelector(".claroEscuro");
@@ -10,6 +11,10 @@ let light = claroEscuro.querySelector("#light");
 
 let header = document.querySelector('header');
 let body = document.querySelector('body');
+let sectionProjetoContainer = document.querySelector(".projetos-container");
+let sectionIntroducao = document.querySelector(".introducao-container");
+let footer = document.querySelector("footer");
+let divCurriculo = sectionIntroducao.querySelector(".curriculo");
 let p = document.querySelectorAll('p');
 
 let a = document.getElementById('navegacao');
@@ -21,17 +26,26 @@ light.addEventListener('click', () => {
     light.classList.add('hidden');
     dark.classList.remove('hidden');
 
-    body.style.backgroundColor = "#ffffff";
-    header.style.backgroundColor = "#CFCFCF";
-    p.forEach(paragraph => { paragraph.style.color = "#c0c0c0";});
+    header.style.backgroundColor = "#ffffff";
+    body.style.backgroundColor = "#f5f5f5";
+    divCurriculo.style.border = "1px solid #000000;"
+    p.forEach(paragraph => { paragraph.style.color = "#4a4a4a";});
     a.style.color = "#F5F5DC";
+    sectionProjetoContainer.style.backgroundColor = "#eaeaea";
+    footer.style.backgroundColor = "#ffffff";
 });
 
 dark.addEventListener('click', () => {
     dark.classList.add('hidden');
     light.classList.remove('hidden');
 
-    body.style.backgroundColor = "#121212";
     header.style.backgroundColor = "#000000";
+    body.style.backgroundColor = "#121212";
+    divCurriculo.style.border = "1px solid #ffffff;"
+    p.forEach(paragraph => { paragraph.style.color = "#b3b3b3";});
     a.style.color = "#b3b3b3";
+    sectionProjetoContainer.style.backgroundColor = "#1A1818";
+    footer.style.backgroundColor = "#000000";
+
 });
+*/
